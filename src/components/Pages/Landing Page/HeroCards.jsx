ea992b7b54112fb5b2213d8f65f219f59b61b0b0
gagan pandey <149556650+gaganpandey1005@users.noreply.github.com>
2025-02-21 18:01:12 +0530

    updates added

--- src/components/Pages/Landing Page/HeroCards.jsx
+++ src/components/Pages/Landing Page/HeroCards.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { delay, motion } from "framer-motion";
 import ED from "../../../assets/images/ED.png"
-const HeroCards = () => {
+import BCE from "../../../assets/images/BCE.png"
+import OOP from "../../../assets/images/OOP.webp"
+const HeroCards = () =>
+{
   const cards = [
-    { id: 1, title: "ED", description: "Engineering Drawing",img:ED },
-    { id: 2, title: "BCE", description: "Basic Civil Engineering" },
-    { id: 3, title: "OOP", description: "Object Oriented Programming" },
-    { id: 4, title: "DSA", description: "Data Strucuture" },
-    
+    { id: 1, title: "PYQ", description: "It contains all pyqs with answer", img: null },
+    { id: 2, title: "Notes", description: "It contains all the Notes", img: null },
+    { id: 3, title: "Books", description: "It contains all the books", img: null},
+    {
+      id: 4, title: "DSA", description: "Data Strucuture"
+    },
+
   ];
 
   return (
     <motion.div
-      initial={{ opacity:0 }}
-      animate={{opacity:1}}
-      transition={{delay:3.6}}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 3.6 }}
       className="overflow-hidden w-full py-10"
     >
       <motion.div
@@ -27,15 +32,19 @@ const HeroCards = () => {
           ease: "linear",
         }}
       >
-        {cards.map((card, index) => (
+        {[...cards, ...cards].map((card, index) => (
           <motion.div
+
             key={index}
-            className="min-w-[250px] text-white md:min-w-[300px] lg:min-w-[350px] bg-gray-900 shadow-lg rounded-xl p-6 text-center border border-gray-200"
+            className="min-w-[200px] text-white md:min-w-[300px] lg:min-w-[418px] bg-gray-900 shadow-lg rounded-xl p-6 text-center border border-gray-200"
             whileHover={{ scale: 1.05 }}
           >
             <img src={card.img} alt="" />
             <h2 className="text-xl  font-semibold ">{card.title}</h2>
-            <p className="">{card.description}</p>
+
+
+            <span className="inline-block text-wrap">{card.description}</span>
+
           </motion.div>
         ))}
       </motion.div>
