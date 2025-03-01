9fcf219a20de7441b3303a94e3ae0617160eb44f
gagan pandey <149556650+gaganpandey1005@users.noreply.github.com>
2025-03-01 13:18:50 +0530

    userController updatd

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,8 +24,8 @@ const register = async (req, res) => {
     const verificationToken = await user.generateVerificationToken();
 
     // ✅ Send Verification Email
-    const verificationLink = `${process.env.BASE_URL}verify-email/${verificationToken}`;
-    await sendEmail(user.email, "Verify Your Email", `Click here to verify your email: ${verificationLink}`);
+    const verificationLink = ${process.env.BASE_URL}verify-email/${verificationToken};
+    await sendEmail(user.email, "Verify Your Email", Click here to verify your email: ${verificationLink});
 
     res.status(200).json({
       success: true,
